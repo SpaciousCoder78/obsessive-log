eb262bfaf1cf2d54b8c112fc7080a8c78bec8664
cssDuke <88923986+cssDuke@users.noreply.github.com>
2022-03-17 17:20:38 +0530

    Add files via upload

---
+++ scripts/script for trigger log.py
@@ -0,0 +1,160 @@
+import xlsxwriter
+#for identifying and logging in triggers
+workbook=xlsxwriter.Workbook("Trigger Log.xlsx")
+worksheet=workbook.add_worksheet("Day 01")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 02")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 03")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+
+worksheet=workbook.add_worksheet("Day 04")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 05")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 06")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 07")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 08")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 09")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 10")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 11")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 12")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 13")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 14")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 15")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 16")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 17")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 18")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 19")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 20")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 21")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 22")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 23")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 24")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 25")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 26")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 27")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 28")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 29")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 30")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+worksheet=workbook.add_worksheet("Day 31")
+worksheet.write("A1","Trigger")
+worksheet.write("B1", "Thought")
+
+
+workbook.close()
